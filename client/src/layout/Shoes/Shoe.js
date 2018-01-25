00c7b1dc3d77b7c812321f236051db8a3f657c30
Nick Nedev <nedevn@gmail.com>
2018-01-24 19:46:20 -0800

    Resized the single shoe image to 200x200

--- client/src/layout/Shoes/Shoe.js
+++ client/src/layout/Shoes/Shoe.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Shoe extends Component {
   render(){
     return (
-      <li>
+      <li className="single-shoe">
         <img src={this.props.shoe.imgUrl} alt=""/> 
 
         <p>Comfort level: {this.props.shoe.comfort}</p>
